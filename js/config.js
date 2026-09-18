b0282d5148da9938228cd820ffe8f3a725b9c5f0
b55250489-dotcom <b55250489@gmail.com>
2026-09-18 16:59:51 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// =============================================
+// OVERTIME — GLOBAL CONFIG
+// =============================================
+
+const OVERTIME_CONFIG = {
+    supabaseUrl: "https://iufyccdxkvhqlcpyxabv.supabase.co",
+
+    supabaseKey:
+        "sb_publishable_9v34ldcSrg3jvCFrdtjcfw_X-o3iXIY",
+
+    twitchParent:
+        "b55250489-dotcom.github.io",
+
+    siteName:
+        "Overtime",
+
+    siteDescription:
+        "Rocket League Esports"
+};
+
+window.OVERTIME_CONFIG = OVERTIME_CONFIG;
